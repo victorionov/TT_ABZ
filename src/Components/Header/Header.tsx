0692c8865c885089d863960580d69d63418235ba
victorionov <victorionovcvlrs@gmail.com>
2023-05-09 16:19:09 +0200

    finished Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.scss';
+
+export const Header: React.FC = () => {
+  return (
+    <div>
+      <div className='header'>
+        <a href="/" className='logo-link'>
+          <img className='logo'src="/logo/logo.png" alt="Website logo" />
+        </a>
+        <div className='buttons'>
+          <button className="button">Users</button>
+          <button className="button">Sign up</button>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
