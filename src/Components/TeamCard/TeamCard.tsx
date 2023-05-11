845c5b09d634586cf968f4a32bdfeead5ce6c671
victorionov <victorionovcvlrs@gmail.com>
2023-05-11 17:25:08 +0200

    teamList & card

--- src/Components/TeamCard/TeamCard.tsx
+++ src/Components/TeamCard/TeamCard.tsx
@@ -3,7 +3,14 @@ import './TeamCard.scss';
 export const TeamCard: React.FC  = () => {
   return (
     <div className="card">
-      this is a card
+      <img className='photo' src="/photo.webp" alt="Person portriet" />
+      <h2 className='person-name'>Salvador Stewart</h2>
+      <div className='person-info'>
+        <p className='person-info--position'>Frontend developer</p>
+        <p className='person-info--email'>Frontend developer@gmail.com</p>
+        <p className='person-info--phone'>+38 (098) 278 44 24</p>
+      </div>
+      
     </div>
   )
 }
